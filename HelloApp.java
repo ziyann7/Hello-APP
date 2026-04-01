109defe0ac7896484dfe691a484c87f9478163ad
ziyann7 <ziyan4doha@gmail.com>
2026-04-01 19:49:26 +0530

    Add files via upload

--- HelloApp.java
+++ HelloApp.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        if (args.length > 0) {
+            String names = String.join(", ", args);
+            System.out.println("Hello " + names + "!");
+        } else {
+            System.out.println("Hello World!");
+        }
     }
 }
